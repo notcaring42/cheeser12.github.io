b4920587e34255b488e48b1d36e279e39b0b8b91
Max Mays <cheeser12@ezen.com>
2019-05-31 09:00:24 -0400

    Add prettier config

--- site.js
+++ site.js
@@ -1,26 +1,24 @@
 window.onscroll = () => {
-    const intro = document.querySelector('.intro-links');
-    const navbar = document.querySelector('.navbar');
+  const intro = document.querySelector(".intro-links");
+  const navbar = document.querySelector(".navbar");
 
-    if (isElementInViewport(intro)) {
-        if (navbar.classList.contains('navbar-fade-in')) {
-            navbar.classList.remove('navbar-fade-in');
-        }
-        navbar.classList.add('navbar-fade-out');
-    } else {
-        if (navbar.classList.contains('navbar-fade-out')) {
-            navbar.classList.remove('navbar-fade-out');
-        }
-
-        navbar.style.display = 'flex';
-        navbar.classList.add('navbar-fade-in');
+  if (isElementInViewport(intro)) {
+    if (navbar.classList.contains("navbar-fade-in")) {
+      navbar.classList.remove("navbar-fade-in");
     }
+    navbar.classList.add("navbar-fade-out");
+  } else {
+    if (navbar.classList.contains("navbar-fade-out")) {
+      navbar.classList.remove("navbar-fade-out");
+    }
+
+    navbar.style.display = "flex";
+    navbar.classList.add("navbar-fade-in");
+  }
 };
 
-function isElementInViewport (el) {
-    const rect = el.getBoundingClientRect();
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
 
-    return (
-        rect.bottom >= 0
-    );
+  return rect.bottom >= 0;
 }
